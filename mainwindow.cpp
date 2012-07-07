0e2316260df1684ab49d042864ca3a0a7b05f6d1
amgits <fpa.777@gmail.com>
2012-07-07 12:25:49 -0300

    Mapa agora é lido da maneira certa, e colisao esta funcionando da maneira básica. Falta implementar empurrar a caixa.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -51,50 +51,35 @@ void MainWindow::keyPressEvent(QKeyEvent *k)
 {
     switch(k->nativeVirtualKey()) {
     case key_left:
-        if( walk(key_left, player) == true )
+        if( canWalk(key_left, player) == true )
         {
             player->setX(player->x()-Block::BLOCK_WIDTH);
         }
-        /*
-        player->setX(player->x()-Block::BLOCK_WIDTH);
-
-        if(collided(player,border.left))
-        {
-            qDebug() << "colidiu";
-            player->setX(player->x()+Block::BLOCK_WIDTH);
-        }*/
         break;
     case key_right:
-        player->setX(player->x()+Block::BLOCK_WIDTH);
-
-        if(collided(player,border.right))
+        if( canWalk(key_right, player) == true )
         {
-            qDebug() << "colidiu";
-            player->setX(player->x()-Block::BLOCK_WIDTH);
+            player->setX(player->x()+Block::BLOCK_WIDTH);
         }
         break;
     case key_up:
-        player->setY(player->y()-Block::BLOCK_HEIGHT);
-
-        if(collided(player,border.top))
+        if( canWalk(key_up, player) == true )
         {
-            qDebug() << "colidiu";
-            player->setY(player->y()+Block::BLOCK_HEIGHT);
+            player->setY(player->y()-Block::BLOCK_HEIGHT);
         }
         break;
     case key_down:
-        player->setY(player->y()+Block::BLOCK_HEIGHT);
-
-        if(collided(player,border.bottom))
+        if( canWalk(key_down, player) == true )
         {
-            qDebug() << "colidiu";
-            player->setY(player->y()-Block::BLOCK_HEIGHT);
+            player->setY(player->y()+Block::BLOCK_HEIGHT);
         }
         break;
     case key_esc:
         this->close();
+        break;
     case key_enter:
         qDebug() << player->x() << player->y();
+        break;
     default:
         break;
     }
@@ -102,10 +87,13 @@ void MainWindow::keyPressEvent(QKeyEvent *k)
 
 void MainWindow::on_actionTest_triggered()
 {
+    m->testScan();
+    /*
     for ( int i=0; i<10; i++ )
     {
         qDebug() << m->mapBuffer[i][0]->type << m->mapBuffer[i][1]->type << m->mapBuffer[i][2]->type << m->mapBuffer[i][3]->type << m->mapBuffer[i][4]->type << m->mapBuffer[i][5]->type << m->mapBuffer[i][6]->type << m->mapBuffer[i][7]->type << m->mapBuffer[i][8]->type << m->mapBuffer[i][9]->type;
     }
+    */
 }
 
 void MainWindow::drawMap()
@@ -114,22 +102,22 @@ void MainWindow::drawMap()
     {
         for(int j=0; j<10; j++)
         {
-            if(m->mapBuffer[j][i]->type == Block::brick)
+            if(m->mapBuffer[i][j]->type == Block::brick)
             {
                 m->mapBuffer[i][j]->square = scene->addRect(0,0,50,50,blackpen,brickbrush);
             }
 
-            if(m->mapBuffer[j][i]->type == Block::box)
+            if(m->mapBuffer[i][j]->type == Block::box)
             {
                 m->mapBuffer[i][j]->square = scene->addRect(0,0,50,50,blackpen,boxbrush);
             }
 
-            if(m->mapBuffer[j][i]->type == Block::spot)
+            if(m->mapBuffer[i][j]->type == Block::spot)
             {
                 m->mapBuffer[i][j]->square = scene->addRect(0,0,50,50,blackpen,spotbrush);
             }
 
-            if(m->mapBuffer[j][i]->type == Block::floor)
+            if(m->mapBuffer[i][j]->type == Block::floor)
             {
                 m->mapBuffer[i][j]->square = scene->addRect(0,0,50,50,blackpen,floorbrush);
             }
@@ -138,32 +126,37 @@ void MainWindow::drawMap()
     }
 }
 
-bool MainWindow::walk(int direction, QGraphicsRectItem* block)
+bool MainWindow::canWalk(int direction, QGraphicsRectItem* block)
 {
     Block *next;
+
     switch(direction)
     {
     case key_left:
         next = m->mapBuffer[(block->x()-50)/50][(block->y())/50];
-        if( next->type == Block::floor )
-        {
-            qDebug() << "pode andar.";
-            return true;
-        }
-        else
-        {
-            qDebug() << next->type;
-            qDebug() << "nao pode andar.";
-            return false;
-        }
         break;
     case key_right:
+        next = m->mapBuffer[(block->x()+50)/50][(block->y())/50];
         break;
     case key_up:
+        next = m->mapBuffer[(block->x())/50][(block->y()-50)/50];
         break;
     case key_down:
+        next = m->mapBuffer[(block->x())/50][(block->y()+50)/50];
         break;
     default:
         break;
+    }
+
+    if( next->type == Block::floor )
+    {
+        qDebug() << "pode andar.";
+        return true;
+    }
+    else
+    {
+        qDebug() << next->type;
+        qDebug() << "nao pode andar.";
+        return false;
     }
 }
